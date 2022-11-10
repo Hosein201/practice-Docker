98fa82bd2d0f5e398249727716c7fa619cf07318
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-11-10 20:43:41 +0330

    app with .net core and sql

---
+++ src/.net Core and Sql/app/aspnetapp/HealthCheckSql.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Data.SqlClient;
+
+namespace DockerApp
+{
+    public class HealthCheckSql : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Server=db;Database=master;User Id=SA;Password=example_123;";
+            try
+            {
+                conn.Open();
+                conn.Close();
+                return Task.FromResult(HealthCheckResult.Healthy());
+            }
+            catch(Exception ex)
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy(ex.Message,ex));
+            }           
+        }
+    }
+}
